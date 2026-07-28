1489ec621626f05c6b7a675c6042786f39b8f5c9
Harshal Chavan <harshal24.chavan@gmail.com>
2026-07-28 09:50:58 +0530

    add flat hash table

---
+++ include/FlatHashTable.hpp
@@ -0,0 +1,73 @@
+#pragma once
+#include <cstdint>
+#include <limits>
+
+constexpr uint64_t EMPTY_SLOT = 0;
+constexpr uint64_t TOMBSTONE = std::numeric_limits<uint64_t>::max();
+constexpr uint32_t NULL_INDEX = 0xFFFFFFFF;
+
+struct alignas(16) HashEntry {
+  uint64_t key;   // will hold the order reference number
+  uint32_t value; // will hold the slab allocators index
+};
+
+class OrderMap {
+private:
+  uint32_t capacity_mask;
+  uint32_t capacity;
+  HashEntry *table;
+
+public:
+  OrderMap(uint32_t capacity_bits = 26) {
+    capacity = 1 << capacity_bits;
+    capacity_mask = capacity - 1;
+
+    table = new HashEntry[capacity]();
+  }
+
+  ~OrderMap() { delete[] table; }
+
+  inline uint32_t hash(uint64_t key) const {
+    key ^= key >> 33;
+    key *= 0xff51afd7ed558ccdULL;
+    key ^= key >> 33;
+    return key & capacity_mask;
+  }
+
+  inline void insert(uint64_t key, uint32_t val) noexcept {
+    uint32_t ind = hash(key);
+    while (true) {
+      if (table[ind].key == EMPTY_SLOT || table[ind].key == TOMBSTONE) {
+        table[ind].key = key;
+        table[ind].value = val;
+        return;
+      }
+      ind = (ind + 1) & capacity_mask;
+    }
+  }
+
+  inline uint32_t get(uint64_t key) const noexcept {
+    uint32_t ind = hash(key);
+    while (table[ind].key != EMPTY_SLOT) {
+      if (table[ind].key == key) {
+        return table[ind].value;
+      }
+      ind = (ind + 1) & capacity_mask;
+    }
+    return NULL_INDEX;
+  }
+
+  inline void erase(uint64_t key) {
+    uint32_t ind = hash(key);
+
+    while (table[ind].key != EMPTY_SLOT) {
+      if (table[ind].key == key) {
+        table[ind].key = TOMBSTONE;
+        table[ind].value = 0;
+        return;
+      }
+      ind = (ind + 1) & capacity_mask;
+    }
+    return;
+  }
+};
