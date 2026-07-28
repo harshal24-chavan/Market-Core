fe1798a86756e20c85c0e55646038af98323276a
Harshal Chavan <harshal24.chavan@gmail.com>
2026-07-28 07:44:06 +0530

    add Slab Allocator

---
+++ include/SlabAllocator.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <vector>
+
+template <typename T, uint32_t SLAB_BITS = 20> class SlabAllocator {
+private:
+  static constexpr uint32_t SLAB_SIZE = 1 << SLAB_BITS;
+  static constexpr uint32_t SLAB_MASK = SLAB_SIZE - 1;
+
+  std::vector<T *> slabs;
+  std::vector<uint32_t> free_list;
+  uint32_t total_capacity = 0;
+
+  void allocate_slab() {
+    T *newSlab = new T[SLAB_SIZE];
+    slabs.push_back(newSlab);
+    uint32_t base_index = total_capacity;
+    uint32_t start_offset = (base_index == 0) ? 1 : 0;
+
+    free_list.reserve(total_capacity + SLAB_SIZE);
+
+    for (uint32_t i = SLAB_SIZE - 1; i >= start_offset; --i) {
+      if (base_index == 0 && i == 0)
+        break;
+
+      // push the new added slab indexes into the free_list
+      free_list.push_back(
+          base_index +
+          i); // but won't this push_back trigger copy when size increases?
+    }
+
+    total_capacity += SLAB_SIZE;
+  }
+
+public:
+  SlabAllocator() { allocate_slab(); }
+
+  ~SlabAllocator() {
+    for (T *slab : slabs) {
+      delete[] slab;
+    }
+  }
+
+  uint32_t allocate() {
+    if (free_list.empty()) {
+      allocate_slab();
+    }
+
+    uint32_t index = free_list.back();
+    free_list.pop_back();
+
+    return index;
+  }
+
+  void free(uint32_t index) { free_list.push_back(index); }
+
+  inline T &get(uint32_t index) {
+    // first 12 bits of index to show the slab
+    // next 20 bits to show the index within the slab
+    uint32_t slab = index >> SLAB_BITS;
+    uint32_t slot_index = index & SLAB_MASK;
+
+    return slabs[slab][slot_index];
+  }
+};
